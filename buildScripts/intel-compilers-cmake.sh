9f101970d166cc3b8ea16cd8070b5f59500e30bf
Damian Rouson <damian@rouson.net>
2013-10-02 13:22:00 -0700

    Updated CMake/make infrastructure, including correcting typos, and added new compiler flags for robustness.

--- buildScripts/intel-compilers-cmake.sh
+++ buildScripts/intel-compilers-cmake.sh
@@ -6,7 +6,7 @@ rm -f CMakeCache.txt
 cmake \
   -D CMAKE_BUILD_TYPE:STRING=DEBUG \
   -D CMAKE_Fortran_COMPILER:FILEPATH=ifort \
-  -D CMAKE_Fortran_FLAGS:STRING="-g" \
+  -D CMAKE_Fortran_FLAGS:STRING="-standard-semantics" \
   -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
   -D ALL_EXAMPLES_ENABLED:BOOL=OFF \
 $EXTRA_ARGS \
--- buildScripts/nag-compilers-cmake.sh
+++ buildScripts/nag-compilers-cmake.sh
@@ -7,7 +7,7 @@ cmake \
   -D CMAKE_BUILD_TYPE:STRING=DEBUG \
   -D CMAKE_CXX_COMPILER:FILEPATH=g++ \
   -D CMAKE_Fortran_COMPILER:FILEPATH=nagfor \
-  -D CMAKE_Fortran_FLAGS:STRING="-lblas -llapack -L/Users/rouson/bin/lapack-3.4.1/" \
+  -D CMAKE_Fortran_FLAGS:STRING="-lblas -llapack -L/Users/rouson/bin/lapack-3.4.1/ -C=all" \
   -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
   -D ALL_EXAMPLES_ENABLED:BOOL=OFF \
 $EXTRA_ARGS \
