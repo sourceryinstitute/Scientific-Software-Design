9843e9170a40cc3f2a2892057ad82203cb7c4d8f
Damian Rouson <damian@rouson.net>
2013-12-16 18:17:27 -0800

    Updated CMake infrastructure to build with PGI and NAG compilers.

--- buildScripts/pgi-compilers-cmake.sh
+++ buildScripts/pgi-compilers-cmake.sh
@@ -5,12 +5,12 @@ EXTRA_ARGS=$@
 rm -f CMakeCache.txt
 cmake \
   -D CMAKE_BUILD_TYPE:STRING=DEBUG \
-  -D CMAKE_CXX_COMPILER:FILEPATH=g++ \
   -D CMAKE_Fortran_COMPILER:FILEPATH=pgfortran \
-  -D CMAKE_Fortran_FLAGS:STRING="-Mallocatable=03 -lblas -llapack -L/Users/rouson/Desktop/lapack-3.4.2/" \
+  -D CMAKE_Fortran_FLAGS:STRING="-Mallocatable=03" \
   -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
   -D ALL_EXAMPLES_ENABLED:BOOL=OFF \
 $EXTRA_ARGS \
 $SSD_PATH
-#  -D CMAKE_Fortran_FLAGS:STRING="-Mallocatable=03 -lblas -L/usr/lib/" \
+# -D CMAKE_Fortran_FLAGS:STRING="-Mallocatable=03 -lblas -L/usr/lib/" \
+# -D CMAKE_CXX_COMPILER:FILEPATH=g++ \
 
